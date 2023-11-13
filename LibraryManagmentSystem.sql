a2e095f843efa446515528d00023f0c3f002aa30
numair <numairshaikh45@gmail.com>
2023-11-13 22:45:20 +0530

    modified

--- LibraryManagmentSystem.sql
+++ LibraryManagmentSystem.sql
@@ -1,2 +1,15 @@
 create database project;
 show databases; 
+use project;
+create table readers
+(
+reader_id varchar(6),
+fname varchar(30),
+mname varchar(30),
+ltname varchar(30),
+city varchar(15),
+mobileno varchar(10),
+occupation varchar(10),
+dob date,
+constraint readers_pk primary key(reader_id)
+);
